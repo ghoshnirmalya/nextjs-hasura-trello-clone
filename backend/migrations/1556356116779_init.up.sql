111c8faa68382d9a249df9ca2498c343b292a5f6
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-04-27 14:41:58 +0530

    feat(backend): Adds migration files

---
+++ backend/migrations/1556356116779_init.up.sql
@@ -0,0 +1,28 @@
+--
+-- SQL edited by https://hasura-edit-pg-dump.now.sh
+--
+
+CREATE TABLE public.board (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text NOT NULL
+);
+
+CREATE TABLE public.list (
+    id uuid DEFAULT public.gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    board_id uuid NOT NULL,
+    "position" integer NOT NULL
+);
+
+ALTER TABLE ONLY public.board
+    ADD CONSTRAINT board_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.list
+    ADD CONSTRAINT list_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.list
+    ADD CONSTRAINT list_position_key UNIQUE ("position");
+
+ALTER TABLE ONLY public.list
+    ADD CONSTRAINT list_board_id_fkey FOREIGN KEY (board_id) REFERENCES public.board(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
