6125a3b7460de8d42d32fd654bca6261a5532941
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-04-28 01:14:05 +0530

    feat(frontend): Adds the option to reorder cards within a list

--- frontend/components/boards/show/index.js
+++ frontend/components/boards/show/index.js
@@ -7,6 +7,7 @@ import Router from 'next/router'
 import Link from 'next/link'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { async } from 'q'
+import find from 'lodash/find'
 
 const fetchBoardSubscription = gql`
   subscription($id: uuid!) {
@@ -16,11 +17,14 @@ const fetchBoardSubscription = gql`
       lists(order_by: { position: asc }) {
         id
         name
+        cards(order_by: { position: asc }) {
+          id
+          description
+        }
       }
     }
   }
 `
-
 const updateListMutation = gql`
   mutation($id: uuid!, $position: Int) {
     update_list(where: { id: { _eq: $id } }, _set: { position: $position }) {
@@ -32,9 +36,20 @@ const updateListMutation = gql`
     }
   }
 `
+const updateCardMutation = gql`
+  mutation($id: uuid!, $position: Int) {
+    update_card(where: { id: { _eq: $id } }, _set: { position: $position }) {
+      returning {
+        id
+        description
+        position
+      }
+    }
+  }
+`
 
 class BoardsShow extends Component {
-  getItemStyle = (isDragging, draggableStyle) => ({
+  getListStyle = (isDragging, draggableStyle) => ({
     // some basic styles to make the items look a bit nicer
     userSelect: 'none',
     // change background colour if dragging
@@ -45,10 +60,10 @@ class BoardsShow extends Component {
   })
 
   listNode = lists => {
-    return lists.map((item, index) => (
+    return lists.map((list, index) => (
       <Draggable
-        key={item.id}
-        draggableId={item.id}
+        key={list.id}
+        draggableId={list.id}
         index={index}
         className="bg-gray-100 min-h-full p-8 border border-solid border-gray-300 rounded"
       >
@@ -57,43 +72,123 @@ class BoardsShow extends Component {
             ref={provided.innerRef}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
-            style={this.getItemStyle(
+            style={this.getListStyle(
               snapshot.isDragging,
               provided.draggableProps.style
             )}
             className="bg-gray-100 p-8 border border-solid border-gray-300 rounded mr-8 w-64"
           >
-            {item.name}
+            {list.name}
+            <Droppable droppableId={list.id} type="card">
+              {(provided, snapshot) => (
+                <div ref={provided.innerRef} className="flex flex-col">
+                  {this.cardNode(list.cards)}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
           </div>
         )}
       </Draggable>
     ))
   }
 
-  onListsDragEnd = async (result, lists) => {
-    const { draggableId, destination, source } = result
+  onDragEnd = async (result, lists) => {
+    const { draggableId, destination, source, type } = result
 
-    /**
-     * Update destination list
-     */
-    await this.props.client.mutate({
-      mutation: updateListMutation,
-      variables: {
-        id: lists[source.index].id,
-        position: destination.index,
-      },
-    })
+    if (!destination) {
+      return false
+    }
 
-    /**
-     * Update source list
-     */
-    await this.props.client.mutate({
-      mutation: updateListMutation,
-      variables: {
-        id: lists[destination.index].id,
-        position: source.index,
-      },
-    })
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return false
+    }
+
+    if (type === 'list') {
+      /**
+       * Update destination list
+       */
+      await this.props.client.mutate({
+        mutation: updateListMutation,
+        variables: {
+          id: lists[source.index].id,
+          position: destination.index,
+        },
+      })
+
+      /**
+       * Update source list
+       */
+      await this.props.client.mutate({
+        mutation: updateListMutation,
+        variables: {
+          id: lists[destination.index].id,
+          position: source.index,
+        },
+      })
+    }
+
+    if (type === 'card') {
+      /**
+       * Card has been reordered within the same list
+       */
+      if (destination.droppableId === source.droppableId) {
+        const list = find(lists, l => l.id === destination.droppableId)
+        const destinationCard = list.cards[destination.index]
+        const sourceCard = list.cards[source.index]
+
+        /**
+         * Update destination card
+         */
+        await this.props.client.mutate({
+          mutation: updateCardMutation,
+          variables: {
+            id: destinationCard.id,
+            position: source.index,
+          },
+        })
+
+        /**
+         * Update source list
+         */
+        await this.props.client.mutate({
+          mutation: updateCardMutation,
+          variables: {
+            id: sourceCard.id,
+            position: destination.index,
+          },
+        })
+      }
+    }
+  }
+
+  cardNode = cards => {
+    return cards.map((card, index) => (
+      <Draggable
+        key={card.id}
+        draggableId={card.id}
+        index={index}
+        className="bg-gray-100 min-h-full p-8 border border-solid border-gray-300 rounded"
+      >
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            style={this.getListStyle(
+              snapshot.isDragging,
+              provided.draggableProps.style
+            )}
+            className="bg-white p-2 border border-solid border-gray-300 rounded my-2 w-full min-h-8 shadow"
+          >
+            {card.description}
+          </div>
+        )}
+      </Draggable>
+    ))
   }
 
   render() {
@@ -112,11 +207,11 @@ class BoardsShow extends Component {
 
           return (
             <DragDropContext
-              onDragEnd={results => this.onListsDragEnd(results, lists)}
+              onDragEnd={results => this.onDragEnd(results, lists)}
             >
               <Droppable droppableId="board" type="list" direction="horizontal">
                 {(provided, snapshot) => (
-                  <div ref={provided.innerRef} className="flex flex-column">
+                  <div ref={provided.innerRef} className="flex">
                     {this.listNode(lists)}
                     {provided.placeholder}
                   </div>
