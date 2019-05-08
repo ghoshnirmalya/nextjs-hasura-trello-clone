1bc5ae4788aef134e3ea32c20184044df5f9edc8
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-05-08 07:21:07 +0530

    refactor(frontend): Removes scroll using css

--- frontend/components/layout/content.tsx
+++ frontend/components/layout/content.tsx
@@ -5,7 +5,7 @@ class ContentComponent extends Component {
     return (
       <div className="m-8">
         <div
-          className="overflow-x-scroll overflow-y-hidden"
+          className="overflow-hidden"
           style={{ height: 'calc(100vh - 130px)' }}
         >
           {this.props.children}
