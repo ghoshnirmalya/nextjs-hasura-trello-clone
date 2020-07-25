b0ae7e7170c48794cad4004b8c94543d4c9a52bc
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-07-25 16:08:54 +0530

    feat: uses custom email for authentication

---
+++ backend/migrations/1595670601846_alter_table_public_users_alter_column_name/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."users" ALTER COLUMN "name" DROP NOT NULL;
--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -21,6 +21,10 @@ const options = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    Providers.Email({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   database: process.env.DATABASE_URL,
   session: {
